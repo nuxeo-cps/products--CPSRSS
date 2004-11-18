364897df2267728d4f8414cfe405952df68570e8
afernandez <devnull@localhost>
2004-11-18 09:52:47 +0000

    deleted tab

--- RSSChannel.py
+++ RSSChannel.py
@@ -256,7 +256,7 @@ class RSSChannel(PortalContent, DefaultDublinCoreImpl):
                         item['title'] = it['title']
                         item['url'] = it['link']
                         item['description'] = it.get('description', '')
-			item['author'] = it.get('author', '')
+                        item['author'] = it.get('author', '')
                         item['modified'] = it.get('modified','')
                         items.append(item)
                 # If the max number of items to be displayed is limited
