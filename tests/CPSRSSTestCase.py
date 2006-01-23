3ae9e107f17b48643901fc8a69824f0c4fe580d2
div <devnull@localhost>
2006-01-23 11:16:20 +0000

    switch to layer based tests

--- tests/CPSRSSTestCase.py
+++ tests/CPSRSSTestCase.py
@@ -1,16 +1,31 @@
-from Testing import ZopeTestCase
+# (C) Copyright 2006 Nuxeo SAS <http://nuxeo.com>
+# Author: Dragos Ivan <fg@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
 
-ZopeTestCase.installProduct('CPSRSS')
-# XXX: needed ?
-#ZopeTestCase.installProduct('PortalTransforms')
-#ZopeTestCase.installProduct('Epoz')
-
-# XXX: change this later so that we try the tool inside a 
-# CMF site, not a CPS.
-from Products.CPSDefault.tests import CPSTestCase
-
-CPSTestCase.setupPortal()
-
-CPSRSSTestCase = CPSTestCase.CPSTestCase
+from Products.CPSDefault.tests.CPSTestCase import CPSTestCase
+from Products.CPSDefault.tests.CPSTestCase import ExtensionProfileLayerClass
 
 
+class LayerClass(ExtensionProfileLayerClass):
+    extension_ids = ('CPSRSS:default',)
+
+CPSRSSLayer = LayerClass(__name__, 'CPSRSSLayer')
+
+
+class CPSRSSTestCase(CPSTestCase):
+    layer = CPSRSSLayer
